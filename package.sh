ee47f6837ef44521b209dffcc7ad23ffd83a0850
Yash-Singh1 <saiansh2525@gmail.com>
2020-12-27 15:32:29 -0800

    Check args instead of args length

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ set -e
 echo -n 'Clearing Workspace...' && rm -rf dist && rm -rf dist.zip && echo 'done' || exit 1
 echo -n 'Copying files...' && mkdir dist && mkdir dist/img && cp {background.js,deinitialize.js,initialize.js,manifest.json,options.css,options.html,options.js,popup.css,popup.html,popup.js} dist && cp img/icon.png dist/img && echo 'done' || exit 1
 
-if [ $# == 0 ]
+if [ "$1" != "--no-zip" ] && [ "$1" != "-n" ]
 then
     echo 'Zipping...' && echo && zip -r dist.zip dist && echo && echo 'DONE!!!' || exit 1
 fi
