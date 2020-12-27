43aa9afd5960be0eb4c526ae02847a709f2326cb
Yash-Singh1 <saiansh2525@gmail.com>
2020-12-27 15:36:46 -0800

    Fix shell script

--- package.sh
+++ package.sh
@@ -6,5 +6,6 @@ echo -n 'Clearing Workspace...' && rm -rf dist && rm -rf dist.zip && echo 'done'
 echo -n 'Copying files...' && mkdir dist && mkdir dist/img && cp {background.js,deinitialize.js,initialize.js,manifest.json,options.css,options.html,options.js,popup.css,popup.html,popup.js} dist && cp img/icon.png dist/img && echo 'done' || exit 1
 
 if [ "$1" != "--no-zip" ] && [ "$1" != "-n" ]
-then  echo 'Zipping...' && echo && zip -r dist.zip dist && echo && echo 'DONE!!!' || exit 1
+then
+  echo 'Zipping...' && echo && zip -r dist.zip dist && echo && echo 'DONE!!!' || exit 1
 fi
